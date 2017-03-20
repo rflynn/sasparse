eceb02ce84d729904df956e4d6cb3d26291dbd7a
Ryan Flynn <parseerror@gmail.com>
2017-03-19 21:37:22 -0400

    skip unit tests if files are unavailable, so we can run unit tests

--- tests/test_parse_nocrash.py
+++ tests/test_parse_nocrash.py
@@ -1,4 +1,5 @@
 
+import os
 import unittest
 
 from nose.tools import assert_raises
@@ -28,117 +29,156 @@ class TestParseDoesntCrash(unittest.TestCase):
     def test_ParseNode_comment_dump(self):
         SASDoc.from_string('*;').dump()
 
+    def test_macro_macro_call_variable(self):
+        SASDoc.from_string('%IF "&LABELMAC" ne "" %THEN %&LABELMAC;  *HCC labels;').dump()
+
     def test_parseerror_str(self):
         assert_raises(Exception, SASDoc.from_string, '"')
 
     def test_parseerror_child(self):
         assert_raises(Exception, SASDoc.from_string, '1 2 3"')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-array.sas'), '')
     def test_parse_array(self):
         SASDoc.from_file('corpus/test-parse-array.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-control.sas'), '')
     def test_parse_control(self):
         SASDoc.from_file('corpus/test-parse-control.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-data.sas'), '')
     def test_parse_data(self):
         SASDoc.from_file('corpus/test-parse-data.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-do.sas'), '')
     def test_parse_do(self):
         SASDoc.from_file('corpus/test-parse-do.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-drop.sas'), '')
     def test_parse_drop(self):
         SASDoc.from_file('corpus/test-parse-drop.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-filename.sas'), '')
     def test_parse_filename(self):
         SASDoc.from_file('corpus/test-parse-filename.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-libname.sas'), '')
     def test_parse_libname(self):
         SASDoc.from_file('corpus/test-parse-libname.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-macros.sas'), '')
     def test_parse_macros(self):
         SASDoc.from_file('corpus/test-parse-macros.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-ods.sas'), '')
     def test_parse_ods(self):
         print(SASDoc.from_file('corpus/test-parse-ods.sas'))
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-options.sas'), '')
     def test_parse_options(self):
         SASDoc.from_file('corpus/test-parse-options.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-proc-cimport.sas'), '')
     def test_parse_proc_cimport(self):
         SASDoc.from_file('corpus/test-parse-proc-cimport.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-proc-datasets.sas'), '')
     def test_parse_proc_datasets(self):
         SASDoc.from_file('corpus/test-parse-proc-datasets.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-proc-export.sas'), '')
     def test_parse_proc_export(self):
         SASDoc.from_file('corpus/test-parse-proc-export.sas')
 
+    @unittest.skipUnless(os.path.exists(''), '')
     def test_parse_proc_format(self):
         SASDoc.from_file('corpus/test-parse-proc-format.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-proc-freq.sas'), '')
     def test_parse_proc_freq(self):
         SASDoc.from_file('corpus/test-parse-proc-freq.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-proc-gmap.sas'), '')
     def test_parse_proc_gmap(self):
         SASDoc.from_file('corpus/test-parse-proc-gmap.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-proc-gremove.sas'), '')
     def test_parse_proc_gremove(self):
         SASDoc.from_file('corpus/test-parse-proc-gremove.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-proc-import.sas'), '')
     def test_parse_proc_import(self):
         print(SASDoc.from_file('corpus/test-parse-proc-import.sas'))
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-proc-means.sas'), '')
     def test_parse_proc_means(self):
         SASDoc.from_file('corpus/test-parse-proc-means.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-proc-print.sas'), '')
     def test_parse_print(self):
         SASDoc.from_file('corpus/test-parse-proc-print.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-proc-rank.sas'), '')
     def test_parse_proc_rank(self):
         SASDoc.from_file('corpus/test-parse-proc-rank.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-proc-report.sas'), '')
     def test_parse_proc_report(self):
         SASDoc.from_file('corpus/test-parse-proc-report.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-proc-sgpanel.sas'), '')
     def test_parse_proc_sgpanel(self):
         SASDoc.from_file('corpus/test-parse-proc-sgpanel.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-proc-sql-select.sas'), '')
     def test_parse_proc_sql_select(self):
         SASDoc.from_file('corpus/test-parse-proc-sql-select.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-proc-sort.sas'), '')
     def test_parse_proc_sort(self):
         SASDoc.from_file('corpus/test-parse-proc-sort.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-proc-transpose.sas'), '')
     def test_parse_proc_transpose(self):
         SASDoc.from_file('corpus/test-parse-proc-transpose.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-proc-template.sas'), '')
     def test_parse_proc_template(self):
         SASDoc.from_file('corpus/test-parse-proc-template.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-proc-univariate.sas'), '')
     def test_parse_proc_univariate(self):
         SASDoc.from_file('corpus/test-parse-proc-univariate.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-signon.sas'), '')
     def test_parse_signon(self):
         SASDoc.from_file('corpus/test-parse-signon.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-title.sas'), '')
     def test_parse_title(self):
         SASDoc.from_file('corpus/test-parse-title.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-toplevel.sas'), '')
     def test_parse_toplevel(self):
         SASDoc.from_file('corpus/test-parse-toplevel.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-var.sas'), '')
     def test_parse_var(self):
         SASDoc.from_file('corpus/test-parse-var.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-sql-create-table.sas'), '')
     def test_parse_sql_create_table(self):
         SASDoc.from_file('corpus/test-parse-sql-create-table.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-sql-create-view.sas'), '')
     def test_parse_sql_create_view(self):
         SASDoc.from_file('corpus/test-parse-sql-create-view.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/test-parse-sql-nonstandard.sas'), '')
     def test_parse_sql_sas_nonstandard(self):
         SASDoc.from_file('corpus/test-parse-sql-nonstandard.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/AC001_P03_Q01_20160628_S01.sas'), '')
     def test_nocrash_bene_characteristics(self):
         SASDoc.from_file('corpus/AC001_P03_Q01_20160628_S01.sas')
 
@@ -147,11 +187,14 @@ class TestDMRCorpus(unittest.TestCase):
 
     _multiprocess_can_split_ = True
 
+    @unittest.skipUnless(os.path.exists('corpus/snf-profiles.sas'), '')
     def test_nocrash_snf_profiles(self):
         SASDoc.from_file('corpus/snf-profiles.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/hospital-profiles.sas'), '')
     def test_nocrash_hospital_profiles(self):
         SASDoc.from_file('corpus/hospital-profiles.sas')
 
+    @unittest.skipUnless(os.path.exists('corpus/awvcclfv6.sas'), '')
     def test_nocrash_awvcclfv6(self):
         SASDoc.from_file('corpus/awvcclfv6.sas')
--- tests/test_symio.py
+++ tests/test_symio.py
@@ -1,4 +1,5 @@
 
+import os
 import unittest
 
 from sasparse import SASDoc
@@ -8,5 +9,6 @@ class TestSymIO(unittest.TestCase):
 
     _multiprocess_can_split_ = True
 
+    @unittest.skipUnless(os.path.exists('corpus/test-symio-001.sas'), '')
     def test_symio_001(self):
         SASDoc.from_file('corpus/test-symio-001.sas')
